647c650b61d06f4e96c27df0f65adaba10193d39
Heitor Farias <heitorfariaspinheirodoamaral@gmail.com>
2025-09-29 22:00:36 +0000

    Att userSchema + authController

--- Back/controllers/authController.js
+++ Back/controllers/authController.js
@@ -3,35 +3,35 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 exports.register = async (req, res) => {
-    const { username, password } = req.body
-    if (!username || !password) return res.status(400).json({ message: 'Por favor, preencha todos os campos.' })
+    const { firstName, lastName, email, CPF, phone, password } = req.body
+    if (!username || !password || !firstName ||!lastName || !email || !CPF || !phone) return res.status(400).json({ message: 'Por favor, preencha todos os campos.' })
 
     try {
-        const userExists = await User.findOne({ username })
+        const userExists = await User.findOne({ CPF })
         if (userExists) return res.status(400).json({ message: 'Usuário já existe.' })
 
         const crip = await bcrypt.hash(password, 10)
-        const user = new User({ username, password: crip })
+        const user = new User({ firstName, lastName, email, CPF, phone, password: crip })
         await user.save()
         res.status(201).json({ message: 'Usuário registrado' })
     } catch (e) {
-        res.status(500).json({ message: 'Erro ao cadastrar usuario' })
+        res.status(500).json({ message: 'Erro ao cadastrar usuário' })
     }
 }
 
 exports.login = async (req, res) => {
-    const { username, password } = req.body
-    if (!username || !password) return res.status(400).json({ message: 'Por favor, preencha todos os campos.' })
+    const { CPF, password } = req.body
+    if (!CPF || !password) return res.status(400).json({ message: 'Por favor, preencha todos os campos.' })
 
     try {
-        const user = await User.findOne({ username })
+        const user = await User.findOne({ CPF })
         if (!user) return res.status(400).json({ message: 'Usuário não encontrado.' })
 
         const validar = await bcrypt.compare(password, user.password)
         if (!validar) return res.status(400).json({ message: 'Senha incorreta.' })
 
         const token = jwt.sign(
-            { id: user._id, username: user.username },
+            { id: user._id, CPF: user.CPF },
             process.env.JWT_SECRET,
             { expiresIn: '1h' }
         )
